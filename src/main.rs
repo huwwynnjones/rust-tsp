c296a42eed441e0cf16df90e65d96c1518c3800a
Huw Wynn-Jones <huw.wynn-jones@maersk.com>
2022-09-30 23:11:10 +0100

    iterable permutations created

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+mod permutation;
+
+use crate::permutation::Permutations;
+
+fn main() {
+    //let result = permutations(&mut vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    //let result = permutations(&mut vec![1, 2, 3]);
+    //println!("{:?}", result);
+    let a = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let perm = Permutations::new(&a);
+    println!("{}", perm.count());
+}
+
+// quickperm see https://www.baeldung.com/cs/array-generate-all-permutations
+// and https://www.quickperm.org/
+fn permutations<T: Clone>(a: &mut [T]) -> Vec<Vec<T>> {
+    let mut result = Vec::new();
+    let n = a.len();
+    let mut p = create_integer_array(n);
+    result.push(a.to_vec());
+    let mut i = 1;
+    while i < n {
+        p[i] -= 1;
+        let j = if odd(i) { p[i] } else { 0 };
+        a.swap(j, i);
+        result.push(a.to_vec());
+        i = 1;
+        while p[i] == 0 {
+            p[i] = i;
+            i += 1;
+        }
+    }
+    result
+}
+
+fn create_integer_array(length: usize) -> Vec<usize> {
+    (0..length + 1).collect::<Vec<usize>>()
+}
+
+fn odd(n: usize) -> bool {
+    n % 2 != 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_integer_array() {
+        let v = create_integer_array(3);
+        assert_eq!(vec![0, 1, 2, 3], v)
+    }
+
+    #[test]
+    fn test_odd() {
+        assert_eq!(odd(1), true);
+        assert_eq!(odd(2), false);
+    }
+
+    #[test]
+    fn test_permutations_char() {
+        let mut a = vec!['a', 'b', 'c'];
+        permutations(&mut a);
+        println!("{:?}", a);
+        //assert_eq!(permutations(&mut a), vec![vec![]])
+    }
+
+    #[test]
+    fn test_permutations_int() {
+        let mut a = vec![1, 2, 3];
+        assert_eq!(
+            permutations(&mut a),
+            vec![
+                vec![1, 2, 3],
+                vec![2, 1, 3],
+                vec![3, 1, 2],
+                vec![1, 3, 2],
+                vec![2, 3, 1],
+                vec![3, 2, 1]
+            ]
+        )
+    }
+}
---
+++ src/permutation.rs
@@ -0,0 +1,73 @@
+pub struct Permutations<T: Clone> {
+    a: Vec<T>,
+    n: usize,
+    p: Vec<usize>,
+    i: usize,
+    initial_call: bool,
+}
+
+impl<T: Clone> Permutations<T> {
+    pub fn new(a: &[T]) -> Self {
+        let a = a.to_vec();
+        let n = a.len();
+        let p = create_integer_array(n);
+        let i = 1;
+        let initial_call = true;
+        Permutations {
+            a,
+            n,
+            p,
+            i,
+            initial_call,
+        }
+    }
+}
+
+impl<T: Clone> Iterator for Permutations<T> {
+    type Item = Vec<T>;
+
+    // quickperm see https://www.baeldung.com/cs/array-generate-all-permutations
+    // and https://www.quickperm.org/
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.initial_call {
+            self.initial_call = false;
+            Some(self.a.to_vec())
+        } else if self.i < self.n {
+            self.p[self.i] -= 1;
+            let j = if odd(self.i) { self.p[self.i] } else { 0 };
+            self.a.swap(j, self.i);
+            self.i = 1;
+            while self.p[self.i] == 0 {
+                self.p[self.i] = self.i;
+                self.i += 1;
+            }
+            Some(self.a.to_vec())
+        } else {
+            None
+        }
+    }
+}
+
+fn create_integer_array(length: usize) -> Vec<usize> {
+    (0..length + 1).collect::<Vec<usize>>()
+}
+
+fn odd(n: usize) -> bool {
+    n % 2 != 0
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_permutations() {
+        let mut p = Permutations::new(&vec![1, 2, 3]);
+        assert_eq!(p.next(), Some(vec![1, 2, 3]));
+        assert_eq!(p.next(), Some(vec![2, 1, 3]));
+        assert_eq!(p.next(), Some(vec![3, 1, 2]));
+        assert_eq!(p.next(), Some(vec![1, 3, 2]));
+        assert_eq!(p.next(), Some(vec![2, 3, 1]));
+        assert_eq!(p.next(), Some(vec![3, 2, 1]));
+        assert_eq!(p.next(), None);
+    }
+}
